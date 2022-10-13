5c5e47c3e4b998107f297560fc1059ad5c1b5a6c
Dmitriy Scherbakov <Dima.scherb@mail.ru>
2022-10-13 18:52:28 +0700

    Добавил определение сервиса ссылки

--- QuitLoad/MainPage.xaml.cs
+++ QuitLoad/MainPage.xaml.cs
@@ -8,16 +8,27 @@ namespace QuitLoad;
 
 public partial class MainPage : ContentPage
 {
-	private VKapi _vkApi; // Нужна инициализация
+	//private VKapi _vkApi; // Нужна инициализация
 	private static YoutubeClient s_youtube;
 
 	public static string savePath { get; private set; } = FileSystem.AppDataDirectory; // ВОТ ТУТ ВАЖНО
+
+	private Service _selected;
+	
+	enum Service
+	{
+		undefined = -1,
+		vk,
+		youtube,
+		pornhub
+	}
 
 	public MainPage()
 	{
 		InitializeComponent();
 		s_youtube = new YoutubeClient();
-		//_vkApi = new VKapi("");
+		//_vkApi = new VKapi(""); // AccesToken надо хранить в каком-то файле чтобы при выходе сохранялся и входе загружался
+		_selected = new Service();
 
 		savePathLabel.Text = savePath;
 		videoFrame.IsVisible = false;
@@ -30,11 +41,47 @@ public partial class MainPage : ContentPage
 
 	private async void OnSearchClicked(object sender, EventArgs e) // Нужно написать определение сервиса введеной ссылки
 	{
+		ServiceDefinition();
+
+		if (_selected != Service.undefined)
+		{
+			serviceLabel.Text = "_selected = " + _selected.ToString();
+			videoFrame.IsVisible = true;
+		}
+		else
+		{
+			videoFrame.IsVisible = false;
+			await DisplayAlert("Ошибка", "Не удалось определить сервис", "OK");
+		}
+	}
+
+	private void OnEditorCompleted(object sender, EventArgs e)
+	{
+
+	}
+
+	private async void ServiceDefinition()
+	{
 		if (searchEditor.Text == null || searchEditor.Text == "")
 		{
 			await DisplayAlert("Ошибка", "Поле URL пусто", "OK");
 			return;
 		}
+		
+		for (int i = 0; i < Enum.GetNames(typeof(Service)).Length; i++)
+		{
+			if (searchEditor.Text.Contains(Enum.GetNames(typeof(Service))[i]))
+			{
+				_selected = (Service)i;
+				return;
+			}
+		}
+		_selected = Service.undefined;
+		
+	}
+
+	private async void YTSearch()
+	{
 		try
 		{
 			var video = await s_youtube.Videos.GetAsync(searchEditor.Text);
@@ -51,11 +98,6 @@ public partial class MainPage : ContentPage
 		{
 			await DisplayAlert("Ошибка", "Не удалось получить информацию о видео", "OK");
 		}
-	}
-
-	private void OnEditorCompleted(object sender, EventArgs e)
-	{
-
 	}
 }
 
